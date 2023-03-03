1ceaa9c757671dc47c992eb6ab11297c9918668d
K.Baleashvar <baleashomega@gmail.com>
2023-03-03 18:18:38 +0530

    Add files via upload

---
+++ basics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class check{
+    public static void Main()
+
+    {
+        string a="It's the same";
+        string b="It's the same";
+        
+        if(a==b){
+            Console.WriteLine("Strings are equal");
+        }
+        else{
+            Console.WriteLine("Stringsa re not equal");
+        }
+    }
+}
+
