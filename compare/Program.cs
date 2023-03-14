34d19bf8add0056f48017dbe9b715db16edf7ade
K.Baleashvar <baleashomega@gmail.com>
2023-03-14 10:27:28 +0530

    Add files via upload

---
+++ compare/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the Value for x");
+        int x=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the Value for y");
+        int y=Convert.ToInt32(Console.ReadLine());
+
+        bool result;
+        result=x<y;
+        Console.WriteLine($"The result of weather Xis less than y is "+result);
+    }
+}
