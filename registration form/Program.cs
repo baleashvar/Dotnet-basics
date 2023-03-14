36d01742cce76e4decbe2864487602bd8591b7be
K.Baleashvar <baleashomega@gmail.com>
2023-03-14 10:40:51 +0530

    Add files via upload

---
+++ registration form/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program1
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your name:");
+        string name=Convert.ToString(Console.ReadLine());
+        
+        Console.WriteLine("Enter your age:");
+        int age=Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter your country:");
+        string country=Convert.ToString(Console.ReadLine());
+
+        Console.WriteLine($"Welcome "+name+". Your age is "+age+" and you are from "+country);
+
+    }
+}
